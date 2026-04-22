7f5db8da68a261c9b5bd223270bfa73c96000830
Vamshi Krishna <vamshikrishna@Vamshis-MacBook-Air.local>
2026-04-22 12:24:44 +0530

    fix: vite base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/Vamshi_Final_Year_Project/",
+  base: '/Vamshi_Final_Year_Project/',
   plugins: [react()],
 })
